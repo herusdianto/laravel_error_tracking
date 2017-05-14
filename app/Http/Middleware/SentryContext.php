4444a5d53684bb875bd013db7e08de9a69b8b11c
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2017-05-14 15:06:05 +0700

    created SentryContext middleware

---
+++ app/Http/Middleware/SentryContext.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SentryContext
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app()->bound('sentry')) {
+            /** @var \Raven_Client $sentry */
+            $sentry = app('sentry');
+
+            $is_logged_in = auth()->check();
+
+            // Add user context
+            if ($is_logged_in) {
+                $user = auth()->user();
+
+                $sentry->user_context([
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ]);
+            }
+        }
+
+        return $next($request);
+    }
+}
