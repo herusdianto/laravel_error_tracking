aeeef2c62b877ba3e418a2b20c04c8e8de8d78a0
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2017-05-14 14:03:05 +0700

    added user feedback

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+    private $sentryID;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -33,7 +35,7 @@ class Handler extends ExceptionHandler
     public function report(Exception $exception)
     {
         if ($this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
+            $this->sentryID = app('sentry')->captureException($exception);
         }
 
         parent::report($exception);
@@ -48,7 +50,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+//        return parent::render($request, $exception);
+
+        return response()->view('errors.500', [
+            'sentryID' => $this->sentryID,
+        ], 500);
     }
 
     /**
